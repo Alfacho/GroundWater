62e9f7bf3f8129f795ddb7259c620cb4ef191d2f
AlSi <primer.alex.si@gmail.com>
2023-04-28 21:08:26 +0200

    Server and clients

--- groundwater_client.cpp
+++ groundwater_client.cpp
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <thread>
 
 #define SERVER_IP "127.0.0.1"
 #define DEFAULT_PORT 1607
@@ -14,11 +15,29 @@
 #define SERVER_CLOSE_CONNECTION_SYMBOL '#'
 #define ERROR_C "CLIENT ERROR: "
 
+int client;
+
 bool is_client_connection_close(const char* msg);
 
-int main(int argc,char const* argv[]) {
-    int client;
+void ClientHandler() {
+    char buffer[BUFFER_SIZE];
+    while (true) {
+        if (is_client_connection_close(buffer)) {
+            break;
+        }
 
+        std::cout << "Server: ";
+        recv(client, buffer, BUFFER_SIZE, 0);
+        std::cout << buffer;
+        
+        if (is_client_connection_close(buffer)) {
+            break;
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main(int argc,char const* argv[]) {
     struct sockaddr_in server_address;
 
     client = socket(AF_INET, SOCK_STREAM, 0);
@@ -39,33 +58,17 @@ int main(int argc,char const* argv[]) {
                     << DEFAULT_PORT << std::endl;
     }
 
+    std::thread Hand(ClientHandler);
+
     char buffer[BUFFER_SIZE];
-
-    std::cout << "=> Waitin for server confirmation..." << std::endl;
-    recv(client, buffer, BUFFER_SIZE, 0);
-    std::cout << "=> Connection established" << std::endl 
-                << "Enter " << SERVER_CLOSE_CONNECTION_SYMBOL
-                << " for end the connection." << std::endl; 
-
     while (true) {
         std::cout << "Client: ";
         std::cin.getline(buffer, BUFFER_SIZE);
         send(client, buffer, BUFFER_SIZE, 0);
-
-        if (is_client_connection_close(buffer)) {
-            break;
-        }
-
-        std::cout << "Server: ";
-        recv(client, buffer, BUFFER_SIZE, 0);
-        std::cout << buffer;
-        
-        if (is_client_connection_close(buffer)) {
-            break;
-        }
-        std::cout << std::endl;
     }
+
     close(client);
+    Hand.join();
     std::cout << "\n GoodBye..." << std::endl;
 
     return 0;
